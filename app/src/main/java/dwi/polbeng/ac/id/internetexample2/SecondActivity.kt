5bb56d76c0045fa8d1b6db25fb92b6be9b8f74f3
mhyraa <muhirarani@gmail.com>
2024-10-14 15:53:09 +0700

    Add commit.

---
+++ app/src/main/java/dwi/polbeng/ac/id/internetexample2/SecondActivity.kt
@@ -0,0 +1,17 @@
+package dwi.polbeng.ac.id.internetexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dwi.polbeng.ac.id.internetexample2.databinding.ActivitySecondBinding
+
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val receiveData = intent.getStringExtra("main_activity_data")
+        binding.textView.text = receiveData
+    }
+}
